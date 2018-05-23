abd319dde4fc289f6f45e5884a0db21d294813c0
Dean Hyde <dean.hyde@usaycompare.com>
2018-05-23 12:46:45 +0100

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,45 +1,36 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
-module.exports = setToken;
-module.exports = usaySession;
+module.exports.checkAuth = function(req, res, next) {
 
-module.exports.usayAuth = function(req, res, next) {
-
-  if (req.session.authenticated)
+  if (req.session !== undefined && req.session.authenticated)
     return next();
 
   res.redirect(process.env.APP_AUTH_MANAGER_NODE_URL + '/login?redirect='+process.env.APP_URL);
 }
 
 
-module.exports.setToken = function() {
-  return function setToken(req, res) {
+module.exports.setToken = function(req, res) {
 
-    // If we have a session ID in our query params, use that as our new session ID and redirect to homepage
-    if (req.query.sessionID) {
-      req.session.regenerate(function(err) {
-        // will have a new session here
-      });
-      // Now redirect to homepage
-      res.redirect('/');
+  // If we have a session ID in our query params, use that as our new session ID and redirect to homepage
+  if (req.query.sessionID !== undefined) {
+    
+    res.cookie('usay_session', decodeURIComponent(req.query.sessionID));
+    // Now redirect to homepage
+    res.redirect('/');
 
-    } else {
-      console.log('failed setting session');
-    }
+  } else {
+    console.log('failed setting session');
   }
+
 }
 
-module.exports.usaySession = function() {
+module.exports.session = function() {
   return session({
     store: new RedisStore({host: 'redis', port: 6379}),
     secret: 'keyboard cat',
     name: 'usay_session',
     resave: false,
     saveUninitialized: false,
-    genid: function(req) {
-      // Set session ID from the sessionID query parameter send from the auth service
-      return req.query.sessionID;
-    },
   })
 }
